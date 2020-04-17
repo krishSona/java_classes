0f645fde665fb5db32e90202d693659411004f3a
krishana25 <krishan.gaur@goibibo.com>
2020-04-18 01:20:28 +0530

    Access method by this and super keyword

---
+++ First_Project/src/pkg2/ByThisNsuper.java
@@ -0,0 +1,19 @@
+package pkg2;
+
+public class ByThisNsuper {
+	public void method() {
+		System.out.println("Parent default method");
+	}
+	public void method1(int a) {
+		System.out.println("Parent 1 parameterized method");
+	}
+	public void method2(int a, int b) {
+		System.out.println("Parent 2 parameterized method");
+	}
+	public void method3(int a, int b, int c) {
+		System.out.println("Parent 3 parameterized method");
+	}
+	public void method4(int a, int b, int c, int d) {
+		System.out.println("Parent 4 parameterized method");
+	}
+}
